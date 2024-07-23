c00f8e49965c120de1f8f064294381a9b44af85a
ggarciab <=>
2024-07-23 10:16:41 -0600

    se agrega la arquitectura base del repositorio de ciudadano

---
+++ src/main/java/mx/gob/tecdmx/directorio_ciudadano/DirectorioCiudadanoApplication.java
@@ -0,0 +1,13 @@
+package mx.gob.tecdmx.directorio_ciudadano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectorioCiudadanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectorioCiudadanoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/gob/tecdmx/directorio_ciudadano/DirectorioCiudadanoApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.gob.tecdmx.directorio_ciudadano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectorioCiudadanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
